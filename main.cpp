75d1fa8ef22668947601b595e0ecf30a008e39c4
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-27 12:21:51 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -13,7 +13,20 @@ struct ListNode {
 class Solution {
  public:
   ListNode *deleteDuplicates(ListNode *head) {
-    // FIXME
+    if (head) {
+      ListNode *prev = head, *curr = head->next;
+      while (curr) {
+        if (prev->val == curr->val) {
+          prev->next = curr->next;
+          delete curr;
+          curr = prev->next;
+        } else {
+          prev = prev->next;
+          curr = curr->next;
+        }
+      }
+    }
+
     return head;
   }
 };
